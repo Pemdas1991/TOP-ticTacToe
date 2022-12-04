5a3b037c4fde7c9da24b89d02a89f87d4cef2937
Peter <Pemdas1991@gmail.com>
2022-12-03 17:41:21 -0700

    Formatting changes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 class Game
-
   def initialize
     @GAMEINPLAY = true
     @p1_symbol = 'X'
@@ -18,7 +15,6 @@ class Game
   end
 
   def play
-
     while @GAMEINPLAY == true
       new_board
       @WINNER = false
@@ -30,69 +26,49 @@ class Game
         get_player_move(@current_symbol)
         clear
         draw_board
-        if @turn >= 5
-          @WINNER = check_winner?
-        end
+        @WINNER = check_winner? if @turn >= 5
         if @WINNER
           puts "#{@current_symbol} IS THE WINNER"
           break
         end
-        if @turn >= 9 && !@WINNER 
-          puts "DRAW!"
+        if @turn >= 9 && !@WINNER
+          puts 'DRAW!'
           @WINNER = true
           break
         end
-        
+
         switch_player(@current_symbol)
       end
       # would you like to play a new game
-        puts "Would you like to play again? y/n"
-        new_game = gets.chomp.downcase
-        if new_game == 'n' || new_game == 'no'
-          break @GAMEINPLAY = false
-        end
+      puts 'Would you like to play again? y/n'
+      new_game = gets.chomp.downcase
+      break @GAMEINPLAY = false if %w[n no].include?(new_game)
     end
   end
 
   def check_winner?
     last_player = @current_symbol
-    if @turn < 5
-      return false
-    end
+    return false if @turn < 5
     # check rows
     # a
-    if @board[0][0] == last_player && @board[0][1] == last_player && @board[0][2] == last_player
-      return true
-    end
+    return true if @board[0][0] == last_player && @board[0][1] == last_player && @board[0][2] == last_player
     # b
-    if @board[1][0] == last_player && @board[1][1] == last_player && @board[1][2] == last_player
-      return true
-    end
+    return true if @board[1][0] == last_player && @board[1][1] == last_player && @board[1][2] == last_player
     # c
-    if @board[2][0] == last_player && @board[2][1] == last_player && @board[2][2] == last_player
-      return true
-    end
+    return true if @board[2][0] == last_player && @board[2][1] == last_player && @board[2][2] == last_player
 
     # check columns
-    if @board[0][0] == last_player && @board[1][0] == last_player && @board[2][0] == last_player
-      return true
-    end
-    if @board[0][1] == last_player && @board[1][1] == last_player && @board[2][1] == last_player
-      return true
-    end
-    if @board[0][2] == last_player && @board[1][2] == last_player && @board[2][2] == last_player
-      return true
-    end
+    return true if @board[0][0] == last_player && @board[1][0] == last_player && @board[2][0] == last_player
+    return true if @board[0][1] == last_player && @board[1][1] == last_player && @board[2][1] == last_player
+    return true if @board[0][2] == last_player && @board[1][2] == last_player && @board[2][2] == last_player
     # check diag
-    if @board[0][0] == last_player && @board[1][1] == last_player && @board[2][2] == last_player
-      return true
-    end
+    return true if @board[0][0] == last_player && @board[1][1] == last_player && @board[2][2] == last_player
     # check anti-Diag
-    if @board[0][2] == last_player && @board[1][1] == last_player && @board[2][0] == last_player
-      return true
-    end
+    return unless @board[0][2] == last_player && @board[1][1] == last_player && @board[2][0] == last_player
 
+    true
   end
+
   def clear
     system('clear')
   end
@@ -122,64 +98,57 @@ class Game
   end
 
   def get_player_move(symbol)
-    
     begin
-      puts "Enter a row and column"
+      puts 'Enter a row and column'
       selection = gets.chomp.split('')
       if selection.empty?
         clear
-        draw_board()
+        draw_board
         puts "Please enter a row and column like 'B2'"
         raise
       end
-        row_down = selection[0].downcase
-        column_int = selection[1].to_i - 1
-        
-      if !input_valid?(row_down, column_int)
+      row_down = selection[0].downcase
+      column_int = selection[1].to_i - 1
+
+      unless input_valid?(row_down, column_int)
         clear
-        draw_board()
+        draw_board
         puts 'Please make sure that the column starts with a, b, or c and the column is between 1 and 3 ex... "A1"'
         raise
       end
 
-      if !valid_move?(row_down, column_int)
+      unless valid_move?(row_down, column_int)
         clear
         draw_board
         puts 'Invalid Move, space is occupied'
         raise
       end
-    rescue => exception
+    rescue StandardError => e
       retry
     end
-      update_square(row_down, column_int,symbol)
+    update_square(row_down, column_int, symbol)
   end
 
-  def input_valid? (row_down, column_int)
-        if row_down == 'a' || row_down == 'b' || row_down == 'c'
-          row = row_down
-        end
-        if column_int >= 0 && column_int < 3 
-          column = column_int
-        end
-        if row == nil || column == nil
-          return false
-        end
-        true
-  end 
+  def input_valid?(row_down, column_int)
+    row = row_down if %w[a b c].include?(row_down)
+    column = column_int if column_int >= 0 && column_int < 3
+    return false if row.nil? || column.nil?
 
-  def valid_move?(row,column)
+    true
+  end
+
+  def valid_move?(row, column)
     row_int = convert_row(row)
-    @board[row_int][column] == ' ' ? true : false
+    @board[row_int][column] == ' '
   end
 
   def switch_player(current_symbol)
-    if current_symbol == @p1_symbol
-      @current_symbol = @p2_symbol
-    else
-      @current_symbol = @p1_symbol
-    end
+    @current_symbol = if current_symbol == @p1_symbol
+                        @p2_symbol
+                      else
+                        @p1_symbol
+                      end
   end
-
 end
 
 test_game = Game.new
